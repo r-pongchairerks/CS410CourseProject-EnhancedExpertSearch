75b8f4618056e96bd6b4e388afa87d909653cec9
Pakhi Gupta <pakhig2@illinois.edu>
2023-11-29 04:44:01 -0500

    adding data extraction

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -9,8 +9,8 @@ visited_pages = set()
 
 # initialize the list of discovered urls
 # with the first page to visit
-# urls = ["https://cs.illinois.edu/about/people/all-faculty"]
-urls = ["https://cs.illinois.edu"]
+urls = ["https://cs.illinois.edu/about/people"]
+#urls = ["https://cs.illinois.edu"]
 
 # until all pages have been visited
 while len(urls) != 0:
@@ -27,27 +27,44 @@ while len(urls) != 0:
     soup = BeautifulSoup(response.content, "html.parser")
 
     link_elements = soup.select("a[href]")
-    # if current_url == 'https://cs.illinois.edu/about/people/all-faculty':
-    #     print("all-facult stuff: ", link_elements)
     
     for link_element in link_elements:
         url = link_element["href"]
 
-        if '/about/people/all-faculty' in url:
+        if '/about/people/all-faculty/department-faculty' in url and "https://cs.illinois.edu" + url not in visited_pages:
             urls.append("https://cs.illinois.edu" + url)
-
-        # if re.match(r"/about/people/all\-faculty/\w", url):
-        #     urls.append("https://cs.illinois.edu" + url)
+        
+        if '/about/people/department-faculty' in url and "https://cs.illinois.edu" + url not in visited_pages:
+            urls.append("https://cs.illinois.edu" + url)
             
-    # print(urls)
-
-    # if current_url is product page
     faculty = {}
     faculty["url"] = current_url
-    # TODO: data extraction HERE
+    faculty["name"] = soup.find("figcaption").text.strip()
+    faculty["research_areas"] = []
+    faculty["research_interests"] = []
+    faculty["education"] = []
+    for ra in soup.find("div", attrs={"class": "extProfileAREA"}).find_all("li"):
+        faculty["research_areas"].append(ra.text)
+    ri_flag = False
+    ed_flag = False
+    for element in soup.find("div", attrs={"class": "directory-profile"}).find_all():
+        if element.text.lower().strip() == "education":
+            ed_flag = True
+            continue
+        if element.text == "Research Interests":
+            ri_flag = True
+            ed_flag = False
+            continue
+        if element.text == "Research Areas":
+            ri_flag = False
+        if ed_flag:
+            faculty["education"].append(element.text.replace("\n", " ").strip())
+
+        if ri_flag and "\n" not in element.text:
+            faculty["research_interests"] += [i.strip() for i in element.text.split(',')]
 
     faculties.append(faculty)
     
-print(faculties)
+#print(faculties)
 
 
